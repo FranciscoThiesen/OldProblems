7ea604b70f72824657a804e546773ce13c608b28
francisco <francisco@MacBook-Pro-de-francisco.local>
2016-01-11 22:46:49 -0200

    Adding new solutions

---
+++ HackerRank/Math/Fundamentals/MinimumDraws.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <iostream>
+
+int main()
+{
+    int t;
+    std::cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        int a;
+        std::cin >> a;
+        std::cout << a + 1 << std::endl;
+    }
+    return 0;
+}
