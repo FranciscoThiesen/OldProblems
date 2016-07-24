0aa98b7dfa7b80a074e45753a067e5e5d2b9968d
francisco <francisco@MacBook-Pro-de-francisco.local>
2016-07-23 21:08:35 -0300

    Organizing problems

--- URI/Problems/1233.cpp
+++ URI/Problems/1233.cpp
@@ -4,18 +4,18 @@ using namespace std;
 long long phi(long long N) {
     long long ans = N;
     if (!(N & 1)) {
-        ans >>= 1;
-        while (!(N & 1))
-            N >>= 1;
-    }
-    for (long long p = 3; p*p <= N; p += 2) {
-        if (N % p == 0) {
-            ans /= p;
-            ans *= (p-1);
-            while (N % p == 0)
-                N /= p;
+            ans >>= 1;
+            while (!(N & 1))
+                N >>= 1;
         }
-    }
+    for (long long p = 3; p*p <= N; p += 2) {
+            if (N % p == 0) {
+                        ans /= p;
+                        ans *= (p-1);
+                        while (N % p == 0)
+                            N /= p;
+                    }
+        }
  
     if (N > 1)
         ans = (ans/N)*(N-1);
@@ -30,3 +30,4 @@ int main() {
         cout << phi(N)/2 << '\n';
     return 0;
 }
+
